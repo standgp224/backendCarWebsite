ce37e45fdd54da9bdbee187c94f0a3b62ea53ace
Tim.Qiu <tqiu1992@gmail.com>
2023-03-17 19:03:55 -0400

    finished eureka and convert pricing-service to microservice

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (vehicle_Id, currency, price) VALUES (1, 'usd','120000');
+INSERT INTO price (vehicle_Id, currency, price) VALUES (2, 'usd','100000');
+INSERT INTO price (vehicle_Id, currency, price) VALUES (3, 'rmb','234555');
+INSERT INTO price (vehicle_Id, currency, price) VALUES (4, 'usd','23441');
+INSERT INTO price (vehicle_Id, currency, price) VALUES (5, 'rmb','300000');
